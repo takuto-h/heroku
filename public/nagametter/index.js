37cfd6e3daddecca4a4e6477600a547b5c898f57
Takuto Hayashi <takuto.hayashi@gmail.com>
2013-06-21 14:31:25 +0900

    Use jQuery

---
+++ public/nagametter/index.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function ($){
+  $("#search").click(function (event){
+    search(query);
+  });
+
+  $("#query").keypress(function (event){
+    if (event.which == 13){
+      event.preventDefault();
+      search(query);
+    }
+  });
+
+  function search(){
+    var query = $("#query").val();
+    if (query == "") {
+      return;
+    }
+    $("#query").attr("disabled", "disabled");
+    $("#search").attr("disabled", "disabled");
+    $.getJSON("search", {q: query}, function (images){
+      addImages(images);
+      $("#query").removeAttr("disabled");
+      $("#search").removeAttr("disabled");
+    });
+  }
+
+  function addImages(images){
+    var p = $("<p></p>");
+    $.each(images, function (index, image_url){
+      p.append($("<img />", {src: image_url}));
+    });
+    $("#images").prepend(p)
+  }
+});
--- web.rb
+++ web.rb
@@ -2,31 +2,26 @@
 
 require 'sinatra'
 require 'twitter'
-require 'haml'
+require 'json'
 
 get '/' do
   "Hello, world"
 end
 
-get '/nagametter' do
+use Rack::Static, root: "public", urls: {
+  "/nagametter" => "/nagametter/index.html",
+  "/nagametter/" => "/nagametter/index.html",
+}
+
+get '/nagametter/search' do
   client = Twitter::Client.new(
     consumer_key: ENV["CONSUMER_KEY"],
     consumer_secret: ENV["CONSUMER_SECRET"],
   )
-  @images = ""
-  client.search(params[:q]).statuses.each do |tweet|
-    @images << haml(
-      "%img{src: tweet.user.profile_image_url}", locals: {tweet: tweet}
-    )
+  profile_image_urls = []
+  client.search("#{params[:q]}").statuses.each do |tweet|
+    profile_image_urls << tweet.user.profile_image_url
   end
-  haml :nagametter
+  content_type :json
+  profile_image_urls.to_json
 end
-
-__END__
-
-@@ nagametter
-%html
-  %head
-    %title Nagametter
-  %body
-    = @images
